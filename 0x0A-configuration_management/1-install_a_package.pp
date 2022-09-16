86e692c8ea8d88ce068ee11849bf0b9bb390b249
flovian <flovian.owiti@gmail.com>
2022-09-16 21:21:59 +0000

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# install flask from pip3
+# installs flask from pip3.
 
-exec { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'flask':
+  ensure   =>  '2.1.0',
+  provider =>  'pip3',
 }
