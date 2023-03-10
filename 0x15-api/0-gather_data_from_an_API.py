560be99f4e92dbaea18e39fd73cc3ddaaa77ac67
flovian <flovian.owiti@gmail.com>
2023-03-10 14:33:02 +0000

    fix task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,18 @@
-
-
 #!/usr/bin/python3
-'''A script that gathers data from an API.
-'''
-import re
+'''returns information about TODO list progress by employee id.'''
 import requests
-import sys
-
-
-API_URL = 'https://jsonplaceholder.typicode.com'
-'''The API's URL.'''
+from sys import argv
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
-            todos_res = requests.get('{}/todos'.format(API_URL)).json()
-            user_name = user_res.get('name')
-            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
-            todos_done = list(filter(lambda x: x.get('completed'), todos))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    user_name,
-                    len(todos_done),
-                    len(todos)
-                )
-            )
-            for todo_done in todos_done:
-                print('\t {}'.format(todo_done.get('title')))
+    data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    data = data.json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = users.json()
+    user = [i for i in users if i.get('id') == int(argv[1])][0]
+    tasks = [i for i in data if i.get('userId') == int(argv[1])]
+    completed = [i for i in tasks if i.get('completed') is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get('name'), len(completed), len(tasks)))
+    for task in completed:
+        print("\t {}".format(task.get('title')))
